63e11a8fade8ba48c27dc8c43be54850d4ccf519
Aileen Rae <aileenlrae@gmail.com>
2026-05-09 18:56:35 +0100

    Rename env variables to match deploy config

--- src/db/supabase.js
+++ src/db/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabaseUrl = import.meta.env.SUPABASE_DATABASE_URL;
 const supabaseKey = import.meta.env.SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
