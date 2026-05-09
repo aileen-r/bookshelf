930e1f83afa95849fca64fb5d3444c5909e697ba
Aileen Rae <aileenlrae@gmail.com>
2026-05-09 16:38:50 +0100

    Add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import vue from '@astrojs/vue';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   // AR: not working somehow
   // integrations: [vue({devtools: true})],
   integrations: [vue()],
+
+  adapter: netlify(),
 });
