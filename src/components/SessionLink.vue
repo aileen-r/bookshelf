c163fb452aff30055abb61944c8935ec29ed43cf
Aileen Rae <aileenlrae@gmail.com>
2026-05-10 17:17:32 +0100

    Add colour ramp variables; basic dark mode for

--- src/components/SessionLink.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <a v-if="loggedIn" href="/api/auth/signout" @click="signOut">Sign Out</a>
-  <a v-else href="/auth/signin">Sign In</a>
-</template>
-
-<script setup>
-const props = defineProps({loggedIn: Boolean});
-</script>
