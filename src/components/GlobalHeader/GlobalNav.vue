e81e5b6f74ccfb2ce938438fa5a99274d86c0746
Aileen Rae <aileenlrae@gmail.com>
2026-05-10 18:56:18 +0100

    WIP adding shadcn-vue as UI library

---
+++ src/components/GlobalHeader/GlobalNav.vue
@@ -0,0 +1,29 @@
+<template>
+  <NavigationMenu>
+    <NavigationMenuList>
+      <NavigationMenuItem v-for="link in links">
+        <NavigationMenuLink :href="link.href">
+          {{ link.name }}
+        </NavigationMenuLink>
+      </NavigationMenuItem>
+    </NavigationMenuList>
+  </NavigationMenu>
+</template>
+<script setup>
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+const links = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+];
+</script>
+
+<style lang="css" scoped>
+nav {
+  font-size: 1rem;
+}
+</style>
---
+++ src/components/common/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <a
+    v-if="href"
+    :href="href"
+    :target="{ external: '_blank' }"
+    :rel="{ external: 'noopener nofollow' }"
+    ><slot></slot
+  ></a>
+  <button v-else><slot></slot></button>
+</template>
+
+<script setup>
+const props = defineProps({ href: [String, null], external: Boolean });
+</script>
+
+<style scoped>
+  a, button {
+    border-radius: 0.5rem;
+    /* border-style: var(--tw-border-style); */
+    /* font-size: var(--text-sm); */
+    /* line-height: var(--tw-leading,var(--text-sm--line-height));
+    --tw-font-weight: var(--font-weight-medium);
+    font-weight: var(--font-weight-medium); */
+    background-clip: padding-box;
+    border-width: 1px;
+    border-color: #0000;
+  }
+</style>
